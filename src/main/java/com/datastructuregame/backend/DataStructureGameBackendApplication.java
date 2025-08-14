4f1b523608db7b1c3429ee99fe8d7b4897d6c2fd
Parkseungje <tmdwprpdla@naver.com>
2025-08-14 23:38:54 +0900

    environment setting

---
+++ src/main/java/com/datastructuregame/backend/DataStructureGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.datastructuregame.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datastructuregame/backend/DataStructureGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datastructuregame.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
